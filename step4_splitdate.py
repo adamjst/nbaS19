1ffb036748404350276c56a599af2cb9d37e7a46
adamjst <30847837+adamjst@users.noreply.github.com>
2019-11-06 06:49:33 +0100

    added column to track months

--- step4_splitdate.py
+++ step4_splitdate.py
@@ -5,21 +5,21 @@ from pathlib import Path
 def df_splitdate():
     """Convert date column to datetime and then return year in new column"""
     ## Set relative path
-    path = Path(r'data/total_data.csv', header=True)
+    path = Path('data', 'total_data.csv', header=True)
     ## Read in csv
     df = pd.read_csv(path, index_col=None, header=0)
+
     ##Convert to datetime
     df['date'] = pd.to_datetime(df['date'])
     df['Year']= df['date'].dt.year
-
-    print(df['Year'])
+    df['Month'] = df['date'].dt.month
 
     #Drop unused columns
     df.drop("Unnamed: 0", axis=1, inplace=True)
     df.drop("Unnamed: 0.1", axis = 1, inplace=True)
     print(df.columns)
     #Write to csv
-    df.to_csv("data/total_data.csv", header=True)
+    df.to_csv(path, header=True)
 
 
 df_splitdate()
