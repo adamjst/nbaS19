473f105d8cb1d6e634fe35b00dff2e7c5133b441
adamjst <30847837+adamjst@users.noreply.github.com>
2019-11-18 06:56:38 +0100

    Update step2_localscrape.py
    Reduced redundancies, added argument to function, still working on bringing in steps 4 and 5...

--- step2_localscrape.py
+++ step2_localscrape.py
@@ -1,15 +1,16 @@
 # Import libraries
 import pandas as pd
+import re
 from bs4 import BeautifulSoup
 from pathlib import Path
 
 """Scrape local html file and return in html and formatted csv"""
 
-def regular_scrape(year_start, years_back):
+def step2_scrape(year_start, years_back, stage):
     """Scrape local html file for regular season data and return in html and csv in separate output folders"""
     counter = 0
     leagues = ['NBA', 'ABA', 'BAA']
-    mths = ['october', 'november', 'december', 'january', 'february', 'march']
+    mths = ['october', 'november', 'december', 'january', 'february', 'march', 'april']
     yrs = []
     yrs.append(year_start)
     for i in range(years_back):
@@ -18,13 +19,21 @@ def regular_scrape(year_start, years_back):
         for yr in yrs:
             for mth in mths:
                 ##Assign local path, URL
-                print(yr, mth)
-                html_folder = Path('data', 'html', '{}_{}_{}.html'.format(league, yr, mth), header=True)
 
-                outputs = Path('data', 'outputs', '{}_{}_{}_processed.csv'.format(league, yr, mth), header=True)
-                #print(html_folder)
+                if stage == 'Playoffs':
+                    # Create relative path
+                    step1_html = Path('data', 'html', '{}_{}_playoffs_step1.html'.format(league, yr), header=True)
+                    step2_csv = Path('data', 'csv', '{}_{}_playoffs_step2.csv'.format(league, yr), header=True)
+
+                else:
+                    step1_html = Path('data', 'html', '{}_{}_{}_step1.html'.format(league, yr, mth), header=True)
+                    step2_csv = Path('data', 'csv', '{}_{}_{}_step2.csv'.format(league, yr, mth), header=True)
+
+                #print(yr, mth)
+
+                #print(step1_html)
                 counter += (100 / (74 * 3 * 9))
-                page_url = open(html_folder)
+                page_url = open(step1_html)
                 #Apply Beautiful Soup to determine columns of new table##
                 soup = BeautifulSoup(page_url, "html.parser")
 
@@ -35,6 +44,14 @@ def regular_scrape(year_start, years_back):
                 # if this month is empty or the page is wrong
                 if not table:
                     continue
+
+                ###TRIED STARTING ALIGNMENT OF COLUMNS HERE###
+                cols = table.find_all('tr')[0]
+
+                header = []
+                for col in cols.find_all('th'):
+                      header.append(col.get_text())
+                print(header)
 
 
                 ## Create boxes for each data point for scraping ##
@@ -49,6 +66,9 @@ def regular_scrape(year_start, years_back):
                 attend = []
                 short_url = 'https://www.basketball-reference.com/boxscores/'
                 round = ['regular'] * 250
+                prog = re.compile('[a-zA-z].+')
+                Year = []
+                Month = []
 
 
                 ## Find and extract relevant data point and then append under appropriate column. For Box Score, extract underlying hyperlink ##
@@ -56,8 +76,11 @@ def regular_scrape(year_start, years_back):
                 for row in table.find_all('tr'):
                     dat = row.find('th')
                     #print(dat)
-                    if dat.get_text() == 'Playoffs':
-                        break
+                    if stage != 'Playoffs':
+                        if dat.get_text() == 'Playoffs':
+                            break
+                        else:
+                            pass
                     dat2 = dat.find('a')
                     date.append(dat2.get_text())
                     #print(date)
@@ -90,210 +113,38 @@ def regular_scrape(year_start, years_back):
                     at = row.find_all('td')[7]
                     attend.append(at.get_text())
                     #print(attend)
-                    df = pd.DataFrame(list(zip(date, start, visitor, ptsv, home, ptsh, box, ot, attend, round)))
+                    df = pd.DataFrame(list(zip(date, start, visitor, ptsv, home, ptsh, box, ot, attend, round, Year, Month)))
+                    print(df)
 
                     #print(df)
-                    df.columns = ['date','start','visitor','visitor.pts','home','home.pts','boxscore','ot','attend', 'round']
 
-                    df.to_csv((outputs), header=True)
-                #print(yr, mth)
+                    ### STILL WORKING ON ALIGNING COLUMNS IN DATA FRAME##
 
-def april_scrape(year_start, years_back):
-    """Scrape local html file for April (a mixed-regular season/playoff month) and return in html and formatted csv"""
-    counter = 0
-    leagues = ['NBA', 'ABA', 'BAA']
-    mths = ['april']
-    yrs = []
-    yrs.append(year_start)
-    for i in range(years_back):
-        yrs.append(yrs[i] - 1)
-    for league in leagues:
-        for yr in yrs:
-            for mth in mths:
-                ##Assign local path, URL
-                # print(year, month)
-                html_folder = Path('data', 'html', '{}_{}_{}.html'.format(league, yr, mth), header=True)
+                    # if prog.match(row['start']):
+                    #      # if correct match, insert the match into the new column
+                    #      visitor.append(str(sta.get_text()))
+                    #      ptsv.append(pv.get_text())
+                    #      home.append(str(ho.get_text()))
+                    #      ptsh.append(ph.get_text())
+                    #      start = []
+                    # else:
+                    #      # if no match, insert filler string into new column
+                    #      pass
 
-                outputs = Path('data', 'outputs', '{}_{}_{}_processed.csv'.format(league, yr, mth), header=True)
-                #print(html_folder)
-                counter += (100 / (74 * 3 * 9))
-                page_url = open(html_folder)
-                #Apply Beautiful Soup to determine columns of new table##
-                soup = BeautifulSoup(page_url, "html.parser")
-                #print(soup.prettify())
+                    df.columns = ['date','start','visitor','visitor.pts','home','home.pts','boxscore', 'ot','attend', 'round', 'Year', 'Month']
 
-                table = soup.find('tbody')
-
-                # if this month is empty or the page is wrong
-                if not table:
-                    continue
+                    ####STILL WORKING ON DATETIME SPLITTING####
 
 
+                    # df['date'] = pd.to_datetime(df['date'])
+                    # df['Year'] = df['date'].dt.year
+                    # df['Month'] = df['date'].dt.month
 
-                ## Create boxes for each data point for scraping ##
-                date = []
-                start = []
-                visitor = []
-                ptsv = []
-                home = []
-                ptsh = []
-                box = []
-                ot = []
-                attend = []
-                short_url ='https://www.basketball-reference.com/boxscores/'
-                round = ['regular'] * 250
+                    ##Convert to datetime
 
 
-                ## Find and extract relevant data point and then append under appropriate column. For Box Score, extract underlying hyperlink ##
+                    df.to_csv((step2_csv), header=True)
 
-                for row in table.find_all('tr'):
-                    dat = row.find('th')
-                    #print(dat)
-                    if dat.get_text() == 'Playoffs':
-                        break
-                    dat2 = dat.find('a')
-                    date.append(dat2.get_text())
-                    #print(date)
-                    sta = row.find_all('td')[0]
-                    start.append(sta.get_text())
-                    #print(start)
-                    vis = row.find_all('td')[1]
-                    visitor.append(str(vis.get_text()))
-                    #print(visitor)
-                    pv = row.find_all('td')[2]
-                    ptsv.append(pv.get_text())
-                    #print(ptsv)
-                    ho = row.find_all('td')[3]
-                    home.append(str(ho.get_text()))
-                    #print(home)
-                    ph = row.find_all('td')[4]
-                    ptsh.append(ph.get_text())
-                    #print(ptsh)
-                    #pg = row.find_all('td')[5]
-                    #a = pg.find('a')
-                    #print(a)
-                    #if a.attrs is True:
-                     #   pass
-                    #else:
-                        #continue
-                    #html = list(a.attrs)[2]
-                    #print(html)
-                    #short_html = html.split('"')[0]
-                    #print(short_html)
-                    box.append(short_url)
-                    #print(box)
-                    over = row.find_all('td')[6]
-                    ot.append(over.get_text())
-                    at = row.find_all('td')[7]
-                    attend.append(at.get_text())
-                    #print(attend)
-                    df = pd.DataFrame(list(zip(date, start, visitor, ptsv, home, ptsh, box, ot, attend, round)))
+step2_scrape(2019, 73, 'Regular')
+step2_scrape(2019, 73, 'Playoffs')
 
-                    #print(df)
-                    df.columns = ['date','start','visitor','visitor.pts','home','home.pts','boxscore','ot','attend', 'round']
-
-##source: https://medium.com/@rtjeannier/pandas-101-cont-9d061cb73bfc
-## Assign to CSV written to specific month and year.
-                    # print(df)
-                    df.to_csv((outputs), header=True)
-
-
-def playoff_local_scrape(year_start, years_back):
-    """Scrape local html file for playoff data and return in html and formatted csv"""
-    counter = 0
-    leagues = ['NBA', 'ABA', 'BAA']
-    yrs = []
-    yrs.append(year_start)
-    for i in range(years_back):
-        yrs.append(yrs[i] - 1)
-    for league in leagues:
-        for yr in yrs:
-                ##Assign local path, URL
-                # print(year, month)
-                html_folder = Path('data', 'html', '{}_{}_playoffs.html'.format(league, yr), header=True)
-
-                outputs = Path('data', 'outputs', '{}_{}_playoffs_processed.csv'.format(league, yr), header=True)
-                #print(html_folder)
-                counter += (100 / (74 * 3 * 9))
-                page_url = open(html_folder)
-                #Apply Beautiful Soup to determine columns of new table##
-                soup = BeautifulSoup(page_url, "html.parser")
-                #print(soup.prettify())
-
-                table = soup.find('tbody')
-
-                # if this month is empty or the page is wrong
-                if not table:
-                    continue
-
-
-
-                ## Create boxes for each data point for scraping ##
-                date = []
-                start = []
-                visitor = []
-                ptsv = []
-                home = []
-                ptsh = []
-                box = []
-                ot = []
-                attend = []
-                short_url = 'https://www.basketball-reference.com/boxscores/'
-                round = ['playoffs'] * 250
-
-
-                ## Find and extract relevant data point and then append under appropriate column. For Box Score, extract underlying hyperlink ##
-
-                for row in table.find_all('tr'):
-                    dat = row.find('th')
-                    #print(dat)
-                    if dat.get_text() == 'Playoffs':
-                        break
-                    dat2 = dat.find('a')
-                    date.append(dat2.get_text())
-                    #print(date)
-                    sta = row.find_all('td')[0]
-                    start.append(sta.get_text())
-                    #print(start)
-                    vis = row.find_all('td')[1]
-                    visitor.append(str(vis.get_text()))
-                    #print(visitor)
-                    pv = row.find_all('td')[2]
-                    ptsv.append(pv.get_text())
-                    #print(ptsv)
-                    ho = row.find_all('td')[3]
-                    home.append(str(ho.get_text()))
-                    #print(home)
-                    ph = row.find_all('td')[4]
-                    ptsh.append(ph.get_text())
-                    #print(ptsh)
-                    pg = row.find_all('td')[5]
-                    a = pg.find('a')
-                    box.append(short_url + a.attrs['href'])
-                    #print(a)
-                    #if a.attrs is True:
-                     #   pass
-                    #else:
-                        #continue
-                    #html = list(a.attrs)[2]
-                    #print(html)
-                    #short_html = html.split('"')[0]
-                    #print(short_html)
-                    #box.append(short_url)
-                    #print(box)
-                    over = row.find_all('td')[6]
-                    ot.append(over.get_text())
-                    #print(ot)
-                    at = row.find_all('td')[7]
-                    attend.append(at.get_text())
-                    #print(attend)
-                    df = pd.DataFrame(list(zip(date, start, visitor, ptsv, home, ptsh, box, ot, attend, round)))
-
-                    #print(df)
-                    df.columns = ['date','start','visitor','visitor.pts','home','home.pts','boxscore','ot','attend', 'round']
-
-                    df.to_csv((outputs), header=True)
-
-regular_scrape(2019, 73)
-playoff_local_scrape(2019, 73)
-april_scrape(2019, 73)
