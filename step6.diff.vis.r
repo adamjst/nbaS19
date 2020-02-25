714f010741594db9d283e84973d10c079f41803d
adamjst <30847837+adamjst@users.noreply.github.com>
2020-02-26 00:26:32 +0100

    Add WNBA to visuals

--- step6.diff.vis.r
+++ step6.diff.vis.r
@@ -5,15 +5,14 @@ library(magrittr)
 
 ##Force numeric and add association column
 out_NBA$Season.Start <- as.numeric(rownames(out_NBA))
-out_NBA$Assoc <- 'NBA'
 out_ABA$Season.Start <- as.numeric(rownames(out_ABA))
-out_ABA$Assoc <- 'ABA'
 out_BAA$Season.Start <- as.numeric(rownames(out_BAA))
-out_BAA$Assoc <- 'BAA'
+out_WNBA$Season.Start <- as.numeric(rownames(out_WNBA))
 
 ##stack dataframes
+BAA_WNBA <- rbind(out_BAA, out_WNBA)
 NBA_ABA <- rbind(out_NBA, out_ABA)
-all_assoc <- rbind(NBA_ABA, out_BAA)
+all_assoc <- rbind(NBA_ABA, BAA_WNBA)
 
 
 
@@ -34,4 +33,21 @@ Assoc_plot <- function(league){
 Assoc_plot(out_NBA)
 Assoc_plot(out_ABA)
 Assoc_plot(out_BAA)
+Assoc_plot(out_WNBA)
 Assoc_plot(all_assoc)
+
+##Plot all associations with unique colors
+all_plot <- function(league){
+  ggplot(league, aes(x=Season.Start, y = TVR.Diff, shape = Assoc, color = Assoc))+
+    geom_point()+
+    geom_errorbar(aes(ymin=TVR.Diff-(TVR.Diff-Min.95), ymax=TVR.Diff+(Max.95-TVR.Diff)))+
+    scale_color_manual(values = c("BAA" = "#fec524", "ABA" = "#8b0000", "NBA" = "#0e2240", "WNBA" = '#244289'))+
+    scale_shape_manual(values = c("BAA" = 15, "ABA" = 17, "NBA" = 19, 'WNBA' = 21))+
+    scale_size_manual(values = c("BAA" = 8, "ABA" = 8, "NBA" = 8, "WNBA" = 8)) +
+    geom_smooth(method = 'lm', se = FALSE)+
+    xlab('Season') +
+    ylab('Difference in Transitivity Violation Rate') +
+    theme_bw()
+}
+all_plot(all_assoc)
+
